a7b343c5215cce8deb37875418ca4aefcc8b35f2
ceereenkoc <111969447+ceereenkoc@users.noreply.github.com>
2022-09-17 20:36:41 +0300

    IfElse

---
+++ proje5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ifelse
+{
+    class ifelse
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın.");
+            else if(time <= 18)
+                Console.WriteLine("İyi günler.");
+            else
+                Console.WriteLine("İyi geceler.");
+
+            string sonuç = time <= 18 ? "İyi günler." : "İyi geceler.";
+            
+            sonuç = time >= 6 && time < 11 ? "Günaydın." : time <= 18 ? "İyi günler." : "İyi geceler.";
+            Console.WriteLine(sonuç);
+        }
+    }
+}
