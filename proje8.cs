5b3c13516eb448230d720571594bd31fffd4915d
ceereenkoc <111969447+ceereenkoc@users.noreply.github.com>
2022-09-18 22:43:58 +0300

    while-foreach

---
+++ proje8.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace whileloop
+{
+    class proje8
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#While#");
+            // 1'den başlayarak console'dan girilen sayıya kadar(sayı dahil) ortalama hesaplayıp console yazdıran program
+           
+            Console.Write("Bir sayı giriniz:");
+            int sayı = int.Parse(Console.ReadLine());
+            int sayaç = 1;
+            int toplam = 0;
+
+            while (sayaç <= sayı)
+            {
+                toplam += sayaç;
+                sayaç ++;
+            }
+
+            Console.WriteLine(toplam/sayı);
+
+            // a'dan z'ye kadar olan tüm harfleri console yazdır.
+
+            char karakter = 'a';
+
+            while (karakter <= 'z')
+            {
+              Console.Write(karakter);
+              karakter ++;  
+            }
+
+            Console.WriteLine("#ForEach#");
+            
+            string[] arabalar = {"BMW", "Ford", "Toyota", "Nissan"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
